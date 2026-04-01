ac416c1e2366ce725bbfb19c1647e7bdd2aaef5a
vldeycold <tubum02@gmail.com>
2026-04-01 20:09:07 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ shareBtn.onclick = () => {
 };
 /* UNDANGAN */
 function joinWhatsAppGroup() {
-    window.open("https://whatsapp.com/channel/0029VbCCA6g1NCrOEI15qK07");
+    window.open("https://whatsapp.com/channel/0029VbCKzuEFXUugctLPPy1D");
 }
 
 function openFacebookPage() {
